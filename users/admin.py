78c7a8c0f939660c834263cff2b69c4a660d336c
Wonseok Jang <jangws@Wonseokui-MacBookPro.local>
2021-03-30 23:42:03 +0900

    #3 USER APP

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Admin 만들어주기, user 모델 만들기, 니꼬가 나중에 설명하기로 함
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """ custom user admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -4,6 +4,33 @@ from django.db import models
 
 
 class User(AbstractUser):
+    Gender_Male = "male"
+    Gender_Female = "female"
+    Gender_Other = "other"
+    Gender_Choices = (
+        (Gender_Male, "male"),
+        (Gender_Female, "female"),
+        (Gender_Other, "other"),
+    )
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_USD = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_USD, "KRW"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(
+        choices=Gender_Choices,
+        max_length=10,
+    )
     bio = models.TextField(
         default=""
     )  # 내가 models에 뭘 쓰든 장고가 알아서 form을 만들어 줄거임. 그리고 장고는 database에 migration과 함께 이 form에 필요한 정보를 요청할 거임
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
